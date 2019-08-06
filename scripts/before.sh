ceaf0efb54b1473865c14be16f8f2f28f9a3b7c0
Mikkel O. Laursen <m@ikkel.dk>
2019-08-06 21:06:50 +0200

    TRY ONCE MORE!!

--- scripts/before.sh
+++ scripts/before.sh
@@ -5,9 +5,7 @@ sudo apt-get install python-software-properties -y
 sudo apt-add-repository ppa:chris-lea/node.js -y
 sudo apt-get update
 sudo apt-get install nodejs -y
-
-# Install nodemon
-# sudo npm install nodemon -g
+sudo apt-get install npm -y
 
 # Install forever module 
 sudo npm install forever -g
