f45366a5230757b4b7cc00895544960c72607df3
Mikkel O. Laursen <m@ikkel.dk>
2019-08-06 20:37:47 +0200

    Revert

--- scripts/after.sh
+++ scripts/after.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-source /home/ubuntu/.zshrc
-
 npm install
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-source /home/ubuntu/.zshrc
-
 forever stopall
 forever start -o /home/ubuntu/bottenanna/out.log -e /home/ubuntu/bottenanna/err.log /home/ubuntu/bottenanna/index.js
