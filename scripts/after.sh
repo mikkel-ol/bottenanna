930534f6121c23b4abce269360731ea5b0d80ad5
Mikkel O. Laursen <m@ikkel.dk>
2019-08-07 19:57:51 +0200

    Fix scripts for good

--- scripts/after.sh
+++ scripts/after.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /home/ubuntu/bottenanna
-npm i
+sudo -u ubuntu -H npm install --production
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-forever stopall
 forever start -o /home/ubuntu/bottenanna/out.log -e /home/ubuntu/bottenanna/err.log /home/ubuntu/bottenanna/index.js
